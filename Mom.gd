9e432d56d282a2cd5fc9f6a96133b7325422c705
NaKyle <nakyle.wright@gmail.com>
2023-02-02 23:34:19 -0600

    Don't need to print wiggle every time.

--- Mom.gd
+++ Mom.gd
@@ -1,7 +1,7 @@
 extends Sprite
 
+# moves our sprite to a random location on the screen
 func wiggle():
-	print("wiggle!")
 	var view_size = get_viewport().size
 	position.x = rand_range(0, view_size.x)
 	position.y = rand_range(0, view_size.y)
